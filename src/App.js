dbc734f509fa3b8f5b967b41fa0d57818109769d
Luciano Gerônimo Lisboa <llisboa@unicad.local>
2019-03-25 14:44:53 -0300

    Exemplo de Redux com react feito.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import List from "./js/components/List"
+import Form from "./js/components/Form";
 
 const App = ()=>(
   <div>
     <div>
+    <Form/>
     <h2>teste</h2>
       <List/>
     </div>
