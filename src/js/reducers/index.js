cdcf22e8d95e790721fb63937e7f0423a7d4b620
Luciano Gerônimo Lisboa <llisboa@unicad.local>
2019-03-25 11:23:18 -0300

    Fazendo o tutorial em https://www.valentinog.com/blog/redux/

---
+++ src/js/reducers/index.js
@@ -0,0 +1,8 @@
+//Inicialização do estado inicial.
+const initialState = {
+    articles: []
+  };
+  function rootReducer(state = initialState, action) {
+    return state;
+  };
+  export default rootReducer;
