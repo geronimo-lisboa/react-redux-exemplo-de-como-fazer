e7b86cd32cf4081eec7d5d89e166151cdb63e901
Luciano Gerônimo Lisboa <llisboa@unicad.local>
2019-03-25 14:14:36 -0300

    Ligando o redux ao react

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import store from './js/store/store'
-import {addArticle} from './js/actions/index';
+import React from "react";
+import List from "./js/components/List"
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.store = store; //Pra esse teste vou criar a store aqui mesmo. O certo seria ela ser global
-    this.addArticle = addArticle; //pega a action que eu vou usar no testew
-    this.store.subscribe( () => console.log('alteração no store'));//Isso aqui dispara quando houver alteração no store
-    console.log(this.store.getState()); // Printa o estado inicial
-    this.store.dispatch(this.addArticle({title:'foo', id:'1'}));//manda uma action pro store
-    console.log(this.store.getState()); // Printa o estado inicial
-  
-    console.log("CONSTRUCTOR");
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
+const App = ()=>(
+  <div>
+    <div>
+    <h2>teste</h2>
+      <List/>
+    </div>
+  </div>
+)
 export default App;
+
+
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,12 +1,14 @@
 import {ADD_ARTICLE} from "../constants/action-types"
 //O estado inicial
 const initialState = {
-    articles: []
+    articles: [{id:1, title:"foobar"}]
 };
 
 function rootReducer(state = initialState, action) {
     if(action.type === ADD_ARTICLE){
-        state.articles.push(action.payload)
+        return Object.assign({}, state, {
+            articles: state.articles.concat(action.payload)
+        })        
     }
     return state;
 };
